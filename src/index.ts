eb34f2dd5886e504cef046f5e7ca3a6e0281a002
sergi <sergioarosafdez@gmail.com>
2021-08-09 23:08:09 +0100

    try

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
 import {MineSweeper} from './MineSweeper/BuscaMinas';
 import {MineRandomizer} from './MineSweeper/MineRandomizer';
-import {MineGridCreator} from "./MineSweeper/MinesGridCreator";
-import {MinePositionCollection} from "./MineSweeper/MinePositions";
 
 const minePositionGenerator = new MineRandomizer()
 const minesweeper = new MineSweeper(minePositionGenerator, 10, 5);
 console.log(minesweeper.getBoard())
 const board = document.getElementById("tablero")
-const minePosition = new MinePositionCollection()
-const grid = new MineGridCreator(minePosition,5).getGrid()
 
 board.innerHTML = ""
 document.oncontextmenu = function () {
     return false
 }
-grid.forEach(row => {
-        row.forEach(column => {
+const grid = minesweeper.getBoard()
+
+grid.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
                 const slot = document.createElement("div")
                 slot.classList.add("casilla")
                 board.append(slot)
+
                 slot.addEventListener("click", () => {
-                    slot.classList.add("open")
-                    if (minePosition.hasMine(column[0][0], row[0][0])) {
+                    minesweeper.openSlot(rowIndex, columnIndex)
+                    if (grid[rowIndex][columnIndex] === "*") {
+                        slot.classList.add("open")
                         slot.classList.add("bomba")
                     }
-                    if(minePosition.nearbyMines(column[0][0], row[0][0]) === 0){
+                    if (grid[rowIndex][columnIndex] === "") {
+                        slot.classList.add("open")
                         slot.classList.add("empty")
                     }
                 })
+
                 slot.addEventListener("auxclick", () => {
+                    minesweeper.toggleFlag(rowIndex, columnIndex)
                     slot.classList.add("bandera")
                 })
             }
